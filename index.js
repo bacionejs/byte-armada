1d93939393a5bb8987da801681110ad423c9c64d
bacionejs <escabosadonna90@gmail.com>
2025-11-03 00:12:43 +0800

    updates

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ document.title="Byte Armada";
 document.body.style.background="white";
 let intro=element("div");
 intro.style="position:fixed;inset:0;font:16 monospace;display:flex;align-items:center;justify-content:center;text-align:center;user-select:none";
-intro.innerHTML="B y t e<br>A r m a d a<br><br><br><br><br>";
+intro.innerHTML="B y t e<br>A r m a d a<br><br><br><br><br>Click to establish multiplayer link<br>Requires two Android Chrome phones on the same wifi";
 intro.onclick=handshake;
 };
