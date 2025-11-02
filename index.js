0192fd42ec4a65062f86eca73b5cd52dc079d8a7
bacionejs <escabosadonna90@gmail.com>
2025-11-03 00:22:06 +0800

    updates

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 window.onload = () => {
-document.title="Byte Armada";
-document.body.style.background="white";
-let intro=element("div");
-intro.style="position:fixed;inset:0;font:16 monospace;display:flex;align-items:center;justify-content:center;text-align:center;user-select:none";
-intro.innerHTML="B y t e<br>A r m a d a<br><br><br><br><br>Click to establish multiplayer link<br>Requires two Android Chrome phones on the same wifi";
-intro.onclick=handshake;
+  document.title = "Byte Armada";
+  document.body.style.background = "white";
+
+  let intro = element("div");
+  intro.style = `
+    position:fixed;
+    inset:0;
+    font:20px monospace;
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+    justify-content:center;
+    text-align:center;
+    user-select:none;
+    line-height:1.4;
+  `;
+
+  intro.innerHTML = `
+    B y t e<br>
+    A r m a d a<br><br><br><br><br>
+    <span style="font-size:8px;line-height:1.2;">
+      Click to establish multiplayer link<br>
+      Requires two Android Chrome phones on the same wifi
+    </span>
+  `;
+
+  intro.onclick = handshake;
 };
